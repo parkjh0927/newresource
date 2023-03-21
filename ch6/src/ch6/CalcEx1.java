d4c8dbc3a7aad4b33616e346ecc00b3ae778bb19
Jun <dbapjh@gmail.com>
2023-03-21 12:20:43 +0900

    new comment

--- ch6/src/ch6/CalcEx1.java
+++ ch6/src/ch6/CalcEx1.java
@@ -4,6 +4,7 @@ public class CalcEx1 {
 
 	public static void main(String[] args) {
 		Calc1 cal = new Calc1();
+		//새로운 주석
 		
 		System.out.println(cal.add(3, 4));
 		System.out.println(cal.sub(3, 4));
